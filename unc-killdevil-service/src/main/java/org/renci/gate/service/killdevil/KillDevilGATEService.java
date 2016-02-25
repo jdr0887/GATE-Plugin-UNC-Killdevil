8245e55feab6d969e77609a91e410ce7772d12ab
Jason Reilly <jdr0887@gmail.com>
2016-02-25 13:34:47 -0500

    using localized submission callable

--- unc-killdevil-service/src/main/java/org/renci/gate/service/killdevil/KillDevilGATEService.java
+++ unc-killdevil-service/src/main/java/org/renci/gate/service/killdevil/KillDevilGATEService.java
@@ -31,7 +31,7 @@ import org.slf4j.LoggerFactory;
  */
 public class KillDevilGATEService extends AbstractGATEService {
 
-    private final Logger logger = LoggerFactory.getLogger(KillDevilGATEService.class);
+    private static final Logger logger = LoggerFactory.getLogger(KillDevilGATEService.class);
 
     private final List<LSFSSHJob> jobCache = new ArrayList<LSFSSHJob>();
 
